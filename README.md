5f894b1ff796a19b405f6ba46c2f1f4051a1cf94
koiku <nmhj767@gmail.com>
2020-02-15 18:09:07 +0200

    2

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Random Colors Generator
+# Random Colors Generator for Linux
+### Setup
++ Clone repo ```git clone git@github.com:koiku/color-generator.git```
++ Go to folder ```cd color-generator```
++ Make the programm accessible from anywhere ```sudo mv clr /bin```
+
+### Usage
+```
+user@user:~$ clr```
+
+### Output
+String that contains random rgb color value
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
 using namespace std;
 
+vector<int> rgb();
+string format_rgb(vector<int> rgb);
+string rgb_to_hex(vector<int> rgb);
+
 int main() {
-	cout << "oooo" << endl;
+	cout << format_rgb(rgb()) << endl;
 
 	return 0;
+}
+
+vector<int> rgb() {
+	vector<int> rgb_arr(3);
+
+	srand(time(NULL));
+	
+	rgb_arr[0] = rand() % 256;
+	rgb_arr[1] = rand() % 256;
+	rgb_arr[2] = rand() % 256;
+
+	return rgb_arr;
+}
+
+string format_rgb(vector<int> rgb) {
+	string formated = "rgb(";
+	formated.append(to_string(rgb[0]));
+	formated.append(", ");
+	formated.append(to_string(rgb[1]));
+	formated.append(", ");
+	formated.append(to_string(rgb[2]));
+	formated.append(")");
+
+	return formated;
 }
